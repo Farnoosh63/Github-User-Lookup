646b98ab70d10ee6f8fd89e9fa3cbb38542972a5
Farnoosh <farnoosh@pdx.edu>
2016-05-28 13:16:21 -0700

    add image

--- js/github-interface.js
+++ js/github-interface.js
@@ -3,23 +3,29 @@ var Repo = require('../js/github.js').Repo;
 $(document).ready(function() {
   var repoObject =new Repo();
   $('#submit').click(function(event) {
+    // $("#solution").reset();
+
     $("#solution").show();
     event.preventDefault();
     var userName = $('#userName').val();
     $('#userName').val("");
     repoObject.getRepo(userName,displayUserName);
     repoObject.getAllRepo(userName, displayDescription);
+    // repoObject.getAllRepo(userName, displayImage);
+
     //refresh the page when the user want to look for another userName
     $('#userName').click(function() {
        window.location.reload();
-      //  alert("reloading...");
+      //  $("#solution").hide();
      });
   });
 });
 
-var displayUserName = function(userName, UserNameData, fullNameData, RepoData) {
+var displayUserName = function(userName, UserNameData, fullNameData,avatarData, RepoData) {
   $('.showUserName').text("Username:" + UserNameData );
   $('.showFullName').text("Full name:" + fullNameData );
+  $('.showImage').append("<img src='"+avatarData+"'>" );
+
 
 };
 
--- js/github.js
+++ js/github.js
@@ -1,37 +1,28 @@
 var apiKey = require('./../.env').apiKey;
+// var parse = require('parse-link-header');
+
 
 exports.Repo = function() {};
 
 exports.Repo.prototype.getRepo =function(userName, displayFunction){
   var accessToken;
   if(apiKey){
-    accessToken = "'https://api.github.com/users/' + userName +'?access_token='+apiKey";
+    accessToken = 'https://api.github.com/users/' + userName +'?access_token='+ apiKey;
   } else{
     accessToken = "https://api.github.com/users/" + userName;
   }
   $.get(accessToken).then(function(response){
-    displayFunction(userName, response.login, response.name, response.repos_url);
-    // displayFullName(userName, response.name);
-
-
-    // var img = response.avatar_url;
-    // console.log(img);
-
-    // $('.showUserName').text("Username:" + response.login );
-
-    //
-    // $('.myCanvas').text("href=" +"'"+img+"'");
+    displayFunction(userName, response.login, response.name,response.avatar_url, response.repos_url);
 
   }).fail(function(error){
     $('.showUserName').text(error.responseJSON.message);
-
   });
 
 };
 exports.Repo.prototype.getAllRepo = function(userName, displayFunction){
   $.get('https://api.github.com/users/'+ userName+ '/repos').then(function(repos_url){
     for(var i = 0; i<repos_url.length; i++){
-    $('.showRepo').append("<br>reposiroty #"+(i+1) + " is " + repos_url[i].name + " And description is: "+ repos_url[i].description);
+    $('.showRepo').append("<br><strong>reposiroty #"+(i+1) + "</strong> is " + repos_url[i].name + " And <em>description is: "+ repos_url[i].description +"</em>");
   }
     }).fail(function(error) {
       $('.showRepo').text(error.responseJSON.message);
