ad7ecfc2d8effdcaa584c5bc4f802f085100cb72
Farnoosh <farnoosh@pdx.edu>
2016-05-28 02:28:58 -0700

    apikey optional finctionality

--- js/github.js
+++ js/github.js
@@ -3,7 +3,13 @@ var apiKey = require('./../.env').apiKey;
 exports.Repo = function() {};
 
 exports.Repo.prototype.getRepo =function(userName, displayFunction){
-  $.get('https://api.github.com/users/' + userName +'?access_token='+apiKey).then(function(response){
+  var accessToken;
+  if(apiKey){
+    accessToken = "'https://api.github.com/users/' + userName +'?access_token='+apiKey";
+  } else{
+    accessToken = "https://api.github.com/users/" + userName;
+  }
+  $.get(accessToken).then(function(response){
     displayFunction(userName, response.login, response.name, response.repos_url);
     // displayFullName(userName, response.name);
 
@@ -26,7 +32,7 @@ exports.Repo.prototype.getAllRepo = function(userName, displayFunction){
   $.get('https://api.github.com/users/'+ userName+ '/repos').then(function(repos_url){
     for(var i = 0; i<repos_url.length; i++){
     $('.showRepo').append("<br>reposiroty #"+(i+1) + " is " + repos_url[i].name + " And description is: "+ repos_url[i].description);
-  } 
+  }
     }).fail(function(error) {
       $('.showRepo').text(error.responseJSON.message);
     });
